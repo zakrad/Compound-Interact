364ef1bbb3cbd831a2e1f27bf33030179dbd8510
saxon <1@1.com>
2022-06-19 23:46:42 +0430

    lendingPart

--- contracts/CompInteract.sol
+++ contracts/CompInteract.sol
@@ -16,7 +16,21 @@ contract CompInteract {
 
     function supply(address cTokenAddress, uint underlyingAmount) external {
         CTokenInterface cToken = CTokenInterface(cTokenAddress);
-        address underlyingaddress = cToken.underlying();
-        
+        address underlyingAddress = cToken.underlying();
+        IERC20(underlyingAddress).approve(cTokenAddress, underlyingAmount);
+        uint result = cToken.mint(underlyingAmount);
+        require(
+            result == 0,
+            'cToken#mint() failed see more detail in ErrorReporter.sol of Compound'
+        );
+    }
+
+    function redeem(address cTokenAddress, uint cTokenAmount) external {
+        cTokenInterface cToken = CTokenInterface(cTokenAddress);
+        uint result = cToken.redeem(cTokenAmount);
+        require(
+            result == 0,
+            'cToken#redeem() failed see more detail in ErrorReporter.sol of Compound'
+        );
     }
 }
